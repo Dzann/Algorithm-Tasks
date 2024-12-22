79e828109abd61dd45608ab3ac5a46bbe1522ba5
Dzann <gidzanmuh@gmail.com>
2024-12-22 19:56:51 +0700

    Tugas 12

--- src/Pertemuan_12/FindMax.java
+++ src/Pertemuan_12/FindMax.java
@@ -4,16 +4,19 @@ public class FindMax {
     public static int findMax(int[] arr, int n, int max) {
         if (n == 0) {
             return max;
-        } else if (arr[n] > max) {
-            max = arr[n];
-        } else if (n > 0) {
-            findMax(arr, n -1 , max);
         }
-       return max;
+        if (n > 0) {
+            if (arr[n] > max) {
+                return findMax(arr, n - 1, arr[n]);
+            } else {
+                return findMax(arr, n - 1, max);
+            }
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
         int max = 0;
 
         int result = findMax(arr, arr.length - 1, max);
